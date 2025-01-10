88cf2b0f9e9deb939500efb9952832c1146ea11f
logantormakim <ltormakim@gmail.com>
2025-01-10 15:18:37 +0000

    1/10/2025 in progress

--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -1,6 +1,14 @@
 public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
-
+    public Train(String carName, String contents, String ogCity, String dest, int w, int m) {
+        carName = name;
+       contents  = product;
+        origin = ogCity;
+        dest = destination;
+        w = weight;
+        m = miles; 
+    }
+    
 
 }
